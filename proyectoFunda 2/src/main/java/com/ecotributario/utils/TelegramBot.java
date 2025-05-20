f9e6c196bba20167d07ec6bb10d2ee2935acf55b
sara-munoz-5 <munozsara@javeriana.edu.co>
2025-05-19 22:53:13 -0500

    feat(telegram): implementar envío de mensajes desde backend usando API de Telegram [#56]

--- proyectoFunda 2/src/main/java/com/ecotributario/utils/TelegramBot.java
+++ proyectoFunda 2/src/main/java/com/ecotributario/utils/TelegramBot.java
@@ -1,5 +1,4 @@
 package com.ecotributario.utils;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -40,3 +39,10 @@ public class TelegramBot {
         }
     }
 }
+/*Done! Congratulations on your new bot. You will find it at t.me/Ecotributario_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+Use this token to access the HTTP API:
+8145286400:AAFdn1uF5M7EAORPvd4Dk4dXJPa9VhRUhZA
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api*/
